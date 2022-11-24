3a90aaf91296ff686ddb3f7e1b56ca232f03ad67
facurevol <facundorevol@outlook.com>
2022-11-24 18:06:59 -0300

    homeworkIII ok

--- 03-JS-III/homework/homework.js
+++ 03-JS-III/homework/homework.js
@@ -8,31 +8,49 @@ function imprimirSumaNumeros() {
     // Imprime la suma de los números del 1 al 10
     // Pista: usa un acumulador
     // Tu código:
+   var num = [1,2,3,4,5,6,7,8,9,10];
+   var suma = num.reduce(function(acc, e){
+    return acc + e;
+   });
+   return suma;
 }
+imprimirSumaNumeros([1,2,3,4,5,6,7,8,9,10]);
 
 function encuentraPares(array){
   // Devuelve un arreglo con los pares encontrados
   // en el arreglo de enteros pasado como parámetro
   // Tu código:
-}
+  var pares = array.filter(e => e%2 == 0)
+   return pares;
+};
+encuentraPares([1,2,3,4,5,6,7,8,9,10]);
 
 function elevaAlCuadrado(array){
   // Devuelve un arreglo con cada número del array
   // elevado al cuadrado
   // Tu código:
-}
+  var arrayAlCuadrado = array.map(e => e ** 2)
+  return arrayAlCuadrado; 
+};
+elevaAlCuadrado([1,2,3,4]);
 
 function sumaArray(array){
   // Devuelve el resultado de sumar todos los elementos
   // de un arreglo de enteros dado
   // Tu código:
+   var suma = array.reduce(function(acc, e){
+    return acc + e;
+   });
+   return suma;
 }
+sumaArray([1,25,38,4]);
 
 function numeroDigitos(num){
   // Devuelve el número de dígitos de un número dado
   // Tu código:
+  return num.toString().length; 
 }
-  
+numeroDigitos(1230);
   
   // No modificar nada debajo de esta línea
   // --------------------------------
